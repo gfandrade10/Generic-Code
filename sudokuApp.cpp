e79bcf0a6b4de0bf1ef1f4166d1495563e612d36
Guilherme Andrade <gfandrade10@hotmail.com>
2021-05-19 10:46:56 -0300

    updates

--- sudokuApp.cpp
+++ sudokuApp.cpp
@@ -14,6 +14,7 @@ bool validSolution(unsigned int board[9][9])
             blocks[var].push_back(board[i][j]);
             buffer1.push_back(board[i][j]);
             buffer2.push_back(board[j][i]);
+            if(board[i][j] < 1 || board[i][j] > 9) return false;
         }   
         blocks.push_back(buffer1);
         blocks.push_back(buffer2);
@@ -33,15 +34,15 @@ bool validSolution(unsigned int board[9][9])
 int main(int argc, char const *argv[])
 {
     unsigned int example1[9][9] = 
-       {{1, 2, 3, 4, 5, 6, 7, 8, 9}, 
-        {2, 3, 1, 5, 6, 4, 8, 9, 7}, 
-        {3, 1, 2, 6, 4, 5, 9, 7, 8}, 
-        {4, 5, 6, 7, 8, 9, 1, 2, 3}, 
-        {5, 6, 4, 8, 9, 7, 2, 3, 1}, 
-        {6, 4, 5, 9, 7, 8, 3, 1, 2}, 
-        {7, 8, 9, 1, 2, 3, 4, 5, 6}, 
-        {8, 9, 7, 2, 3, 1, 5, 6, 4}, 
-        {9, 7, 8, 3, 1, 2, 6, 4, 5}};
+       {{8, 2, 6, 3, 4, 7, 5, 9, 1},
+        {7, 3, 5, 8, 1, 9, 6, 4, 2},
+        {1, 9, 4, 2, 6, 5, 8, 7, 3},
+        {3, 1, 7, 5, 8, 4, 2, 6, 9},
+        {6, 5, 9, 1, 7, 2, 4, 3, 8},
+        {4, 8, 2, 9, 3, 6, 7, 1, 5},
+        {9, 4, 8, 7, 5, 1, 3, 2, 6},
+        {5, 6, 1, 4, 2, 3, 9, 8, 7},
+        {2, 7, 3, 6, 9, 8, 1, 5, 4}};
 
     if(validSolution(example1)) std::cout << "TRUE\n";
     else std::cout << "FALSE\n";
